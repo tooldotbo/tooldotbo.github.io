67158efaf60180e17ddcaa9d10dd9208f6bd3ae6
goowenbo <goowenbo@qq.com>
2019-01-25 15:16:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,17 +7,7 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
     , $ = layui.jquery
     , laytpl = layui.laytpl;
 
-
-  //statr 分页
-
-  laypage.render({
-    elem: 'test1' //注意，这里的 test1 是 ID，不用加 # 号
-    , count: 50 //数据总数，从服务端得到
-    , theme: '#1e9fff'
-  });
-
-  // end 分頁
-
+    
   // start 导航显示隐藏
 
   $("#mobile-nav").on('click', function () {
